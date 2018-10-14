fbf04f4131f7cd1bd6cf1f573697b9a0757d0ff0
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2018-10-13 18:29:26 -0700

    Fixed html and js bugs

--- COMP_1170/examples/test_1/index.js
+++ COMP_1170/examples/test_1/index.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-  alert(“page is loaded and ready”);
-});
+document.addEventListener('DOMContentLoaded', function(){ 
+    alert("page is loaded and ready");
+}, false);
+ 
 function changeParagraphContent() {
-var element = document.getElementById("demo");
-if element.classList.contains("red") {
-element.innerHTML = "This page contains some simple html and css";
-element.classList.remove("red");
-} else {
-element.innerHTML = "This is the new paragraph text";
-element.classList.add("red");
+  var element = document.getElementById("demo");
+  if(element.classList.contains("red")) {
+    element.innerHTML = "This page contains some simple html and css";
+    element.classList.remove("red");
+  } else {
+    element.innerHTML = "This is the new paragraph text";
+    element.classList.add("red");
+  }
 }
-}
-
