17df111f96f877b89be50bebf2e08aee8ce53383
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-03-20 21:47:25 -0700

    changed jquery of carousel to js

--- term2-winter2019/dynamic-content-design-MDIA2109/dynamic_content_midterm_assets/apple_products_website/js/carousel.js
+++ term2-winter2019/dynamic-content-design-MDIA2109/dynamic_content_midterm_assets/apple_products_website/js/carousel.js
@@ -1,45 +1,45 @@
 function Carousel() {
-  var leftArrowBtn = $("#main .arrow_left_image");
-  var rightArrowBtn = $("#main .arrow_right_image");
+  var leftArrowBtn = document.querySelector("#main .arrow_left_image");
+  var rightArrowBtn = document.querySelector("#main .arrow_right_image");
  
-  leftArrowBtn.click(function(){
-    var h3 = $("#display h3");
-    var img = $("#display img");
+  leftArrowBtn.addEventListener("click", function(){
+    var h3 = document.querySelector("#display h3");
+    var img = document.querySelector("#display img");
     var product;
-    var descriptionDiv = $("#description_container");
-    var imgDiv = $("#image_holder");
-    var showSpecsBtn = $("#specs_and_price");
-    var specsTextDiv = $("#specs_price_text");
+    var descriptionDiv = document.querySelector("#description_container");
+    var imgDiv = document.querySelector("#image_holder");
+    var showSpecsBtn = document.querySelector("#specs_and_price");
+    var specsTextDiv = document.querySelector("#specs_price_text");
     if(state > 0){
       state = state - 1;
       product = products[state];
-      h3.html(product["name"]);
-      img.attr("src", product.url);
-      descriptionDiv.html("");
-      imgDiv.removeClass("center");
-      showSpecsBtn.addClass("hide");
-      specsTextDiv.html("");
+      h3.innerHTML = product["name"];
+      img.src = product.url;
+      descriptionDiv.innerHTML = "";
+      imgDiv.classList.remove("center");
+      showSpecsBtn.classList.add("hide");
+      specsTextDiv.innerHTML = "";
       showMoreState = false;
     }
   });
   
-  rightArrowBtn.click(function(){
-    var h3 = $("#display h3");
-    var img = $("#display img");
+  rightArrowBtn.addEventListener("click", function(){
+    var h3 = document.querySelector("#display h3");
+    var img = document.querySelector("#display img");
     var product;
-    var descriptionDiv = $("#description_container");
-    var imgDiv = $("#image_holder");
-    var showSpecsBtn = $("#specs_and_price");
-    var specsTextDiv = $("#specs_price_text");
+    var descriptionDiv = document.querySelector("#description_container");
+    var imgDiv = document.querySelector("#image_holder");
+    var showSpecsBtn = document.querySelector("#specs_and_price");
+    var specsTextDiv = document.querySelector("#specs_price_text");
     if(state < (products.length - 1)){
       state = state + 1;
       product = products[state];
-      h3.html(product["name"]);
-      img.attr("src", product.url);
-      descriptionDiv.html("");
-      imgDiv.removeClass("center");
-      showSpecsBtn.addClass("hide");
-      specsTextDiv.html("");
+      h3.innerHTML = product["name"];
+      img.src = product.url;
+      descriptionDiv.innerHTML = "";
+      imgDiv.classList.remove("center");
+      showSpecsBtn.classList.add("hide");
+      specsTextDiv.innerHTML = "";
       showMoreState = false;
     }
   });
