465c90b3508f8fce1c913f08f2cf176480d80c52
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-04-16 12:02:13 -0700

    added files in portfolio folder

---
+++ term2-winter2019/Web_Development/web_dev_winter_2019/Portfolio_assets/portfolio-web-dev-midterm/js/app.js
@@ -0,0 +1,11 @@
+function scaleImage() {
+  var myImage = document.querySelector("#myimage");
+  myImage.addEventListener("mouseenter", function () {
+    myImage.style.transform = "scale(1.5,1.5)";
+  });
+  myImage.addEventListener("mouseout", function(){
+    myImage.style.transform = "scale(1,1)";
+  })
+}
+
+scaleImage();
