40279ace9ce355d29f365f897ba062bd086b802a
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-05-07 12:12:17 -0700

    additions/modifications

--- term2-winter2019/dynamic-content-design-MDIA2109/review_3/js/app.js
+++ term2-winter2019/dynamic-content-design-MDIA2109/review_3/js/app.js
@@ -9,20 +9,24 @@ function App() {
     fontColor: "",
     status: ""
   };
-  
-  var x = {
-    foo: "bar"
-  };
-  var handlers = {
-    get: function(obj, prop) {
-      var original = obj[prop];
-      var res = original.charAt(0).toUpperCase() + original.slice(1, original.length - 1);
-      return res;
-    },
-    set: function(obj, prop, val) {
-      obj[prop] = val;
-    }
-  };
-  var p = new Proxy(x, handlers)
-  p.foo = "baz";
+}
+
+// exponent(5, 10) -> 15625 -> 5, 5*5, 5*5*5, 5*5*5*5, ...
+function exponent(num, limit) {
+  var result = 1;
+  for(var i = 0; i < limit; i++) {
+    result = result * num;
+    console.log(i, result);
+  }
+  return result;
+}
+
+// arithmeticProgression(10) -> 55 -> 1, 1+2, 1+2+3, 1+2+3+4 ...
+function arithmeticProgression(limit) {
+  var res = 0;
+  for(var i=0; i < limit; i++) {
+    res = res + (i +1);
+    console.log(i, res);
+  }
+  return res;
 }
