5d62c4b7e9ef59bb062fadd1e03a2b5d48fee26a
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-05-09 21:44:51 -0700

    completed till question 5 start

--- term2-winter2019/dynamic-content-design-MDIA2109/review_3/js/app.js
+++ term2-winter2019/dynamic-content-design-MDIA2109/review_3/js/app.js
@@ -1,6 +1,9 @@
-function App() {
-
-  var pkg = {
+var pkgdata = window.localStorage.getItem("pkgData");
+var pkg;
+if (pkgdata != null && pkgdata != undefined) {
+  pkg = JSON.parse(pkgdata);
+} else {
+  pkg = {
     fontSize: 5,
     imgDim: 50,
     imgUrl: "",
@@ -11,22 +14,100 @@ function App() {
   };
 }
 
-// exponent(5, 10) -> 15625 -> 5, 5*5, 5*5*5, 5*5*5*5, ...
-function exponent(num, limit) {
-  var result = 1;
-  for(var i = 0; i < limit; i++) {
-    result = result * num;
-    console.log(i, result);
+
+var handler = {
+  get: function (obj, prop) {
+    return obj[prop];
+  },
+  set: function (obj, prop, value) {
+    obj[prop] = value;
+    var jsonString = JSON.stringify(obj);
+    window.localStorage.setItem("pkgData", jsonString);
+    ChangePreviewUI(prop, value);
   }
-  return result;
 }
 
-// arithmeticProgression(10) -> 55 -> 1, 1+2, 1+2+3, 1+2+3+4 ...
-function arithmeticProgression(limit) {
-  var res = 0;
-  for(var i=0; i < limit; i++) {
-    res = res + (i +1);
-    console.log(i, res);
+var prox = new Proxy(pkg, handler);
+
+function ChangePreview(el) {
+  if (el.id == "fontSize") {
+    prox["fontSize"] = el.value;
   }
-  return res;
+  if (el.id == "imgDim") {
+    prox["imgDim"] = el.value;
+  }
+  if (el.id == "imgUrl") {
+    prox["imgUrl"] = el.value;
+  }
+  if (el.id == "msg") {
+    prox["msg"] = el.value;
+  }
+  if (el.id == "bgColor") {
+    prox["bgColor"] = el.value;
+  }
+  if (el.id == "fontColor") {
+    prox["fontColor"] = el.value;
+  }
+
+  if (prox["fontSize"] > 50) {
+    prox["status"] = "Your fonts are too big, use a smaller size";
+  }
 }
+
+
+
+function ChangePreviewUI(property, val) {
+
+  var imageElement = document.getElementById("previewImg");
+  var messageSpan = document.getElementById("previewMsg");
+  var previewDiv = document.getElementById("preview");
+  var statusDiv = document.getElementById("status");
+  
+  if (property == "bgColor") {
+    previewDiv.style.backgroundColor = val;
+  }
+  if (property == "msg") {
+    messageSpan.innerHTML = val;
+  }
+  if (property == "fontSize") {
+    messageSpan.style.fontSize = val + "pt";
+  }
+  if (property == "imgUrl") {
+    console.log(property, val);
+    imageElement.src = val;
+  }
+  if (property == "imgDim") {
+    imageElement.style.width = val + "px";
+    imageElement.style.height = val + "px";
+  }
+  if (property == "fontColor") {
+    console.log(property, val);
+    messageSpan.style.color = val;
+  }
+  if (property == "status") {
+    statusDiv.innerHTML = val;
+  }
+}
+
+
+function MakeClone() {
+  var boardDiv = document.getElementById("board");
+  var previewDiv = document.getElementById("preview");
+
+
+  var newDivInsideBoardDiv = document.createElement("div");
+  newDivInsideBoardDiv.innerHTML = previewDiv.innerHTML;
+  newDivInsideBoardDiv.style.backgroundColor = previewDiv.style.backgroundColor;
+  boardDiv.appendChild(newDivInsideBoardDiv);
+}
+
+function DrawInitialUiState() {
+  ChangePreviewUI("bgColor", prox["bgColor"]);
+  ChangePreviewUI("fontSize", prox["fontSize"]);
+  ChangePreviewUI("imgDim", prox["imgDim"]);
+  ChangePreviewUI("imgUrl", prox["imgUrl"]);
+  ChangePreviewUI("msg", prox["msg"]);
+  ChangePreviewUI("fontColor", prox["fontColor"]);
+  ChangePreviewUI("status", prox["status"]);
+}
+DrawInitialUiState();
